1576848f4ef0fbe0808fe10027524175b18e9a13
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-08-17 19:53:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 //your JS code here. If required.
+// Function to download an image
+function downloadImage(image) {
+    return new Promise((resolve, reject) => {
+        const imgElement = new Image();
+        imgElement.src = image.url;
+
+        imgElement.onload = () => resolve(imgElement);
+        imgElement.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
+    });
+}
+
+// Event listener for the "Download Images" button
+btn.addEventListener("click", () => {
+    const imagePromises = images.map(downloadImage);
+
+    Promise.all(imagePromises)
+        .then(imageElements => {
+            // Clear existing content in the output div
+            output.innerHTML = '';
+
+            // Append image elements to the output div
+            imageElements.forEach(imageElement => {
+                output.appendChild(imageElement);
+            });
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
+
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
